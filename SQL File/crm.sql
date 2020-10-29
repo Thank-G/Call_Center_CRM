2910bef29ec147ecccbc7a3616a1b453a44b81cf
Thank-G <praisejahwithmetb@gmail.com>
2020-10-29 18:23:42 +0100

    first commit

---
+++ SQL File/crm.sql
@@ -0,0 +1,226 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+--
+-- Database: `crm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prequest`
+--
+
+CREATE TABLE `prequest` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `contactno` varchar(11) DEFAULT NULL,
+  `company` varchar(255) DEFAULT NULL,
+  `wdd` varchar(255) DEFAULT NULL,
+  `cms` varchar(255) DEFAULT NULL,
+  `seo` varchar(255) DEFAULT NULL,
+  `smo` varchar(255) DEFAULT NULL,
+  `swd` varchar(255) DEFAULT NULL,
+  `dwd` varchar(255) DEFAULT NULL,
+  `fwd` varchar(255) DEFAULT NULL,
+  `dr` varchar(255) DEFAULT NULL,
+  `whs` varchar(255) DEFAULT NULL,
+  `wm` varchar(255) DEFAULT NULL,
+  `ed` varchar(255) DEFAULT NULL,
+  `wta` varchar(255) DEFAULT NULL,
+  `opi` varchar(255) DEFAULT NULL,
+  `ld` varchar(255) DEFAULT NULL,
+  `da` varchar(255) DEFAULT NULL,
+  `osc` varchar(255) DEFAULT NULL,
+  `nd` varchar(255) DEFAULT NULL,
+  `others` varchar(255) DEFAULT NULL,
+  `query` longtext,
+  `posting_date` date DEFAULT NULL,
+  `remark` longtext,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `prequest`
+--
+
+INSERT INTO `prequest` (`id`, `name`, `email`, `contactno`, `company`, `wdd`, `cms`, `seo`, `smo`, `swd`, `dwd`, `fwd`, `dr`, `whs`, `wm`, `ed`, `wta`, `opi`, `ld`, `da`, `osc`, `nd`, `others`, `query`, `posting_date`, `remark`, `status`) VALUES
+(1, 'ThankGod Bereonwu', 'bereonwuthankgod@gmail.com', '09044945341', 'Treasurehall Consulting', 'Website Design & Development', '', '', '', '', '', '', '', '', 'Website Maintenance', '', '', '', '', '', '', '', '', 'Testing the form', '2020-10-29', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id` int(11) NOT NULL,
+  `ticket_id` varchar(11) NOT NULL,
+  `email_id` varchar(300) NOT NULL,
+  `subject` varchar(300) NOT NULL,
+  `task_type` varchar(300) NOT NULL,
+  `prioprity` varchar(300) NOT NULL,
+  `ticket` longtext NOT NULL,
+  `attachment` varchar(300) NOT NULL,
+  `status` varchar(300) NOT NULL,
+  `admin_remark` longtext NOT NULL,
+  `posting_date` date NOT NULL,
+  `admin_remark_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ticket`
+--
+
+INSERT INTO `ticket` (`id`, `ticket_id`, `email_id`, `subject`, `task_type`, `prioprity`, `ticket`, `attachment`, `status`, `admin_remark`, `posting_date`, `admin_remark_date`) VALUES
+(11, '4', 'thankgod@gmail.com', 'Test Ticket', 'ot1', 'important', 'This is sample text for testing.', '', 'Open', 'Your query is solved now.', '2020-10-29', '2020-10-29 11:58:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `alt_email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `user_image` varchar(255) DEFAULT NULL,
+  `address` varchar(500) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `posting_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `alt_email`, `password`, `mobile`, `gender`, `user_image`, `address`, `status`, `posting_date`) VALUES
+(1, 'Thank-G', 'thankgod@treasurehall.net', '', 'Test@123', '07031984586', 'm', NULL, '21 Rd, Festac, Lagos', NULL, '2020-10-29 10:58:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usercheck`
+--
+
+CREATE TABLE `usercheck` (
+  `id` int(11) NOT NULL,
+  `logindate` varchar(255) DEFAULT '',
+  `logintime` varchar(255) DEFAULT '',
+  `user_id` int(11) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT '',
+  `ip` varbinary(16) DEFAULT NULL,
+  `mac` varbinary(16) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `usercheck`
+--
+
+INSERT INTO `usercheck` (`id`, `logindate`, `logintime`, `user_id`, `username`, `email`, `ip`, `mac`, `city`, `country`) VALUES
+(1, '2020/10/29', '04:54:44pm', 1, 'ABc', 'abc@gmail.com', 0x3a3a31, 0x31322d46342d38442d31322d39392d39, '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `prequest`
+--
+ALTER TABLE `prequest`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usercheck`
+--
+ALTER TABLE `usercheck`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `prequest`
+--
+ALTER TABLE `prequest`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `usercheck`
+--
+ALTER TABLE `usercheck`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
+COMMIT;
+
+
